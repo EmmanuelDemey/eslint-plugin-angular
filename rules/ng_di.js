ba5f388bcf740fae299bd692a078d5c136f2eca6
Emmanuel DEMEY <demey.emmanuel@gmail.com>
2015-02-06 13:19:43 +0100

    Refactor the ng_di rule with the new utility utils module

--- rules/ng_di.js
+++ rules/ng_di.js
@@ -2,6 +2,8 @@ module.exports = function(context) {
 
     'use strict';
 
+    var utils = require('./utils/utils');
+
     var angularObjectList = ['value', 'constant', 'factory', 'service', 'provider', 'controller', 'filter', 'directive'];
 
     function report(node, name, syntax){
@@ -18,11 +20,11 @@ module.exports = function(context) {
             var callee = node.callee;
 
             if (callee.type === 'MemberExpression' && callee.object.name === 'angular' && angularObjectList.indexOf(callee.property.name) >= 0) {
-               if(syntax === 'function' && node.arguments[1].type !== 'FunctionExpression'){
-					report(node, node.arguments[0].value, syntax);
+               if(syntax === 'function' && !utils.isFunctionType(node.arguments[1])){
+                  report(node, node.arguments[0].value, syntax);
                }
-               if(syntax === 'array' && node.arguments[1].type !== 'ArrayExpression'){
-					report(node, node.arguments[0].value, syntax);
+               if(syntax === 'array' && !utils.isArrayType(node.arguments[1])){
+                  report(node, node.arguments[0].value, syntax);
                }
             }
         }
