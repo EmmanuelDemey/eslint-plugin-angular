474f85f8e55e1c3f5e9b7b851229c91ba49ee57c
Emmanuel DEMEY <demey.emmanuel@gmail.com>
2015-02-07 15:18:00 +0100

    Issue #10 Check AngularJS Module name with a Regexp

--- rules/ng_module_name.js
+++ rules/ng_module_name.js
@@ -2,13 +2,7 @@ module.exports = function(context) {
 
     'use strict';
 
-    function report(node, name, prefix){
-
-        context.report(node, 'The {{module}} module should be prefixed by {{prefix}}', {
-            module: name,
-            prefix: prefix
-        });
-    }
+    var utils = require('./utils/utils');
 
     function isArray(node){
         return node !== undefined && node.type === 'ArrayExpression';
@@ -22,10 +16,18 @@ module.exports = function(context) {
             if (callee.type === 'MemberExpression' && callee.property.name === 'module') {
                var name = node.arguments[0].value;
 
-               if(isArray(node.arguments[1]) && name !== undefined && !(name.indexOf(prefix) === 0)){
+               if(isArray(node.arguments[1]) && name !== undefined && !utils.isRegexp(prefix) && !(name.indexOf(prefix) === 0)){
 
-                    report(node, name, prefix);
+                   context.report(node, 'The {{module}} module should be prefixed by {{prefix}}', {
+                        module: name,
+                        prefix: prefix
+                    });
 
+                } else if(isArray(node.arguments[1]) && utils.isRegexp(prefix) && !prefix.test(name)){
+                    context.report(node, 'The {{module}} module should follow this pattern: {{prefix}}', {
+                        module: name,
+                        prefix: prefix.toString()
+                    });
                 }
             }
         }
--- test/ng_module_name.js
+++ test/ng_module_name.js
@@ -14,21 +14,26 @@ eslintTester.addRuleTest('rules/ng_module_name', {
     valid: [{
         code: 'angular.module("eslintModule", []);',
         args: [1, 'eslint']
-    },
-    {
+    }, {
         code: 'angular.module("module");',
         args: [1, 'eslint']
+    }, {
+        code: 'angular.module("eslintModule", []);',
+        args: [1, /^eslint/]
     }],
     invalid: [
         {
             code: 'angular.module("module", []);',
             args: [1, 'eslint'],
             errors: [{ message: 'The module module should be prefixed by eslint'}]
-        },
-        {
+        }, {
             code: 'angular.module("ESLintModule", []);',
             args: [1, 'eslint'],
             errors: [{ message: 'The ESLintModule module should be prefixed by eslint'}]
+        }, {
+            code: 'angular.module("module", []);',
+            args: [1, /^eslint/],
+            errors: [{ message: 'The module module should follow this pattern: /^eslint/'}]
         }
     ]
 });
