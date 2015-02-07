2ee17a190c4fbc2bb280fae8a224965001829fb6
Emmanuel DEMEY <demey.emmanuel@gmail.com>
2015-02-07 15:18:13 +0100

    Issue #10 Check AngularJS Service name with a Regexp

--- rules/ng_service_name.js
+++ rules/ng_service_name.js
@@ -2,13 +2,7 @@ module.exports = function(context) {
 
     'use strict';
 
-    function report(node, name, prefix){
-
-        context.report(node, 'The {{service}} service should be prefixed by {{prefix}}', {
-            service: name,
-            prefix: prefix
-        });
-    }
+    var utils = require('./utils/utils');
 
     return {
 
@@ -20,8 +14,16 @@ module.exports = function(context) {
                 (callee.property.name === 'provider' || callee.property.name === 'service' || callee.property.name === 'factory' || callee.property.name === 'constant' || callee.property.name === 'value')) {
                 var name = node.arguments[0].value;
 
-               if(name !== undefined && !(name.indexOf(prefix) === 0)){
-                    report(node, name, prefix);
+               if(name !== undefined && !utils.isRegexp(prefix) && !(name.indexOf(prefix) === 0)){
+                    context.report(node, 'The {{service}} service should be prefixed by {{prefix}}', {
+                        service: name,
+                        prefix: prefix
+                    });
+                } else if(utils.isRegexp(prefix) && !prefix.test(name)){
+                    context.report(node, 'The {{service}} service should follow this pattern: {{prefix}}', {
+                        service: name,
+                        prefix: prefix.toString()
+                    });
                 }
 
             }
--- test/ng_service_name.js
+++ test/ng_service_name.js
@@ -8,75 +8,34 @@ var eslint = require('../node_modules/eslint/lib/eslint'),
 //------------------------------------------------------------------------------
 // Tests
 //------------------------------------------------------------------------------
+var valid = [], invalid = [];
+['service', 'factory', 'provider', 'constant', 'value'].forEach(function(syntax){
+    valid.push({
+        code: 'angular.' + syntax + '("eslintDirective", function(){});',
+        args: [1, 'eslint']
+    }, {
+        code: 'angular.' + syntax + '("eslintDirective", function(){});',
+        args: [1, /^eslint/]
+    });
+
+    invalid.push({
+        code: 'angular.' + syntax + '("Service", function(){});',
+        args: [1, 'eslint'],
+        errors: [{ message: 'The Service service should be prefixed by eslint'}]
+    }, {
+        code: 'angular.' + syntax + '("esLintService", function(){});',
+        args: [1, 'eslint'],
+        errors: [{ message: 'The esLintService service should be prefixed by eslint'}]
+    }, {
+        code: 'angular.' + syntax + '("Service", function(){});',
+        args: [1, /^eslint/],
+        errors: [{ message: 'The Service service should follow this pattern: /^eslint/'}]
+    });
+});
+
 
 var eslintTester = new ESLintTester(eslint);
 eslintTester.addRuleTest('rules/ng_service_name', {
-    valid: [{
-        code: 'angular.service("eslintDirective", function(){});',
-        args: [1, 'eslint']
-    }, {
-        code: 'angular.factory("eslintDirective", function(){});',
-        args: [1, 'eslint']
-    }, {
-        code: 'angular.provider("eslintDirective", function(){});',
-        args: [1, 'eslint']
-    }, {
-        code: 'angular.constant("eslintDirective", function(){});',
-        args: [1, 'eslint']
-    }, {
-        code: 'angular.value("eslintDirective", function(){});',
-        args: [1, 'eslint']
-    }],
-    invalid: [
-        {
-            code: 'angular.service("Service", function(){});',
-            args: [1, 'eslint'],
-            errors: [{ message: 'The Service service should be prefixed by eslint'}]
-        },
-        {
-            code: 'angular.service("esLintService", function(){});',
-            args: [1, 'eslint'],
-            errors: [{ message: 'The esLintService service should be prefixed by eslint'}]
-        },
-        {
-            code: 'angular.provider("Service", function(){});',
-            args: [1, 'eslint'],
-            errors: [{ message: 'The Service service should be prefixed by eslint'}]
-        },
-        {
-            code: 'angular.provider("esLintService", function(){});',
-            args: [1, 'eslint'],
-            errors: [{ message: 'The esLintService service should be prefixed by eslint'}]
-        },
-        {
-            code: 'angular.factory("Service", function(){});',
-            args: [1, 'eslint'],
-            errors: [{ message: 'The Service service should be prefixed by eslint'}]
-        },
-        {
-            code: 'angular.factory("esLintService", function(){});',
-            args: [1, 'eslint'],
-            errors: [{ message: 'The esLintService service should be prefixed by eslint'}]
-        },
-        {
-            code: 'angular.constant("Service", function(){});',
-            args: [1, 'eslint'],
-            errors: [{ message: 'The Service service should be prefixed by eslint'}]
-        },
-        {
-            code: 'angular.constant("esLintService", function(){});',
-            args: [1, 'eslint'],
-            errors: [{ message: 'The esLintService service should be prefixed by eslint'}]
-        },
-        {
-            code: 'angular.value("Service", function(){});',
-            args: [1, 'eslint'],
-            errors: [{ message: 'The Service service should be prefixed by eslint'}]
-        },
-        {
-            code: 'angular.value("esLintService", function(){});',
-            args: [1, 'eslint'],
-            errors: [{ message: 'The esLintService service should be prefixed by eslint'}]
-        }
-    ]
+    valid: valid,
+    invalid: invalid
 });
