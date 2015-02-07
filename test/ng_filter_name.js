13804895e2df948fd2df4cc45dd9bb81d23292c8
Emmanuel DEMEY <demey.emmanuel@gmail.com>
2015-02-07 15:17:39 +0100

    Issue #10 Check AngularJS Filter name with a Regexp

--- test/ng_filter_name.js
+++ test/ng_filter_name.js
@@ -14,17 +14,23 @@ eslintTester.addRuleTest('rules/ng_filter_name', {
     valid: [{
         code: 'angular.filter("eslintFilter", function(){});',
         args: [1, 'eslint']
+    }, {
+        code: 'angular.filter("eslintFilter", function(){});',
+        args: [1, /^eslint/]
     }],
     invalid: [
         {
             code: 'angular.filter("Filter", function(){});',
             args: [1, 'eslint'],
             errors: [{ message: 'The Filter filter should be prefixed by eslint'}]
-        },
-        {
+        }, {
             code: 'angular.filter("esLintFilter", function(){});',
             args: [1, 'eslint'],
             errors: [{ message: 'The esLintFilter filter should be prefixed by eslint'}]
+        }, {
+            code: 'angular.filter("Filter", function(){});',
+            args: [1, /^eslint/],
+            errors: [{ message: 'The Filter filter should follow this pattern: /^eslint/'}]
         }
     ]
 });
