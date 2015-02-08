a1fd39b186675d07ecf36b498c54c51ed08fdc0e
Emmanuel DEMEY <demey.emmanuel@gmail.com>
2015-02-08 14:32:26 +0100

    Issue #16 ng_window_service rule should return error for window.[document, setTimeout, setInterval]

--- rules/ng_window_service.js
+++ rules/ng_window_service.js
@@ -6,10 +6,11 @@ module.exports = function(context) {
         context.report(node, 'You should use the $window service instead of the default window object', {});
     }
 
+    var restrict = ['document', 'setInterval', 'setTimeout'];
     return {
 
         'MemberExpression': function(node) {
-            if(node.object.name === 'window'){
+            if(node.object.name === 'window' && restrict.indexOf(node.property.name) < 0){
                 report(node);
             }
         }
--- test/ng_window_service.js
+++ test/ng_window_service.js
@@ -13,7 +13,11 @@ var eslintTester = new ESLintTester(eslint);
 eslintTester.addRuleTest('rules/ng_window_service', {
     valid: [
         '$window.location.href = ""',
-        '$window.alert("")'
+        '$window.alert("")',
+        'window.setInterval(function(){}, 0)',
+        'window.setTimeout(function(){}, 0)',
+        'window.document', 
+        'window.document.title'
     ],
     invalid: [
         { code: 'window.alert("")', errors: [{ message: 'You should use the $window service instead of the default window object'}] },
