156e337f9f100011beb4e4284573462dcbb74b7a
Emmanuel DEMEY <demey.emmanuel@gmail.com>
2015-02-26 21:11:39 +0100

    Issue #31 Should use angular.isDefined() instead of !angular.isUndefined

--- rules/ng_definedundefined.js
+++ rules/ng_definedundefined.js
@@ -6,6 +6,19 @@ module.exports = function(context) {
     *  Rule that check if we use angular.is(Un)defined() instead of the undefined keyword
     */
     return {
+        'MemberExpression': function(node){
+            if(node.object.name === 'angular' 
+                && node.parent !== undefined && node.parent.parent !== undefined && node.parent.parent.operator === '!'){
+
+
+                    if(node.property.name === 'isDefined'){
+                        context.report(node, 'Instead of !angular.isDefined, you can use the out-of-box angular.isUndefined method', {});
+                    } else if(node.property.name === 'isUndefined'){
+                        context.report(node, 'Instead of !angular.isUndefined, you can use the out-of-box angular.isDefined method', {});
+                    }
+
+            }
+        },
         'BinaryExpression': function(node) {
 
             if(node.operator === '===' || node.operator === '!=='){
--- test/ng_definedundefined.js
+++ test/ng_definedundefined.js
@@ -19,6 +19,8 @@ eslintTester.addRuleTest('rules/ng_definedundefined', {
         { code: 'variable === undefined', errors: [{ message: 'You should not use directly the "undefined" keyword. Prefer angular.isUndefined or angular.isDefined'}] },
         { code: 'undefined === variable', errors: [{ message: 'You should not use directly the "undefined" keyword. Prefer angular.isUndefined or angular.isDefined'}] },
         { code: 'undefined !== variable', errors: [{ message: 'You should not use directly the "undefined" keyword. Prefer angular.isUndefined or angular.isDefined'}] },
-        { code: 'variable !== undefined', errors: [{ message: 'You should not use directly the "undefined" keyword. Prefer angular.isUndefined or angular.isDefined'}] }
+        { code: 'variable !== undefined', errors: [{ message: 'You should not use directly the "undefined" keyword. Prefer angular.isUndefined or angular.isDefined'}] },
+        { code: '!angular.isUndefined(variable)', errors: [{ message: 'Instead of !angular.isUndefined, you can use the out-of-box angular.isDefined method'}] },
+        { code: '!angular.isDefined(variable)', errors: [{ message: 'Instead of !angular.isDefined, you can use the out-of-box angular.isUndefined method'}] }
     ]
 });
