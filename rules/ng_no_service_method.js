2e2750be053789529b6d1c85db4332d9aac44b14
Emmanuel DEMEY <demey.emmanuel@gmail.com>
2015-03-08 20:07:52 +0100

    Issue #42 Singletons (Y040)

---
+++ rules/ng_no_service_method.js
@@ -0,0 +1,22 @@
+module.exports = function(context) {
+
+    'use strict';
+
+    var utils = require('./utils/utils');
+
+    return {
+
+        'CallExpression': function(node) {
+
+            var prefix = context.options[0];
+            if(prefix === undefined) {
+                return;
+            }
+
+            if (utils.isAngularComponent(node) && node.callee.property.name === 'service') {
+                context.report(node, 'You should prefer the factory() method instead of service()', {});
+            }
+        }
+    };
+
+};
---
+++ test/ng_no_service_method.js
@@ -0,0 +1,38 @@
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+var eslint = require('../node_modules/eslint/lib/eslint'),
+    ESLintTester = require('eslint-tester');
+
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+var valid = [], invalid = [];
+['factory', 'provider', 'constant', 'value'].forEach(function(syntax){
+    valid.push({
+        code: 'app.' + syntax + '("eslintService", function(){});',
+        args: [1, 'eslint']
+    }, {
+        code: 'app.' + syntax + '("eslintService", function(){});',
+        args: [1, /^eslint/]
+    }, {
+        code: 'app.' + syntax + '("eslintService", function(){});',
+        args: [1, undefined]
+    });
+});
+
+
+var eslintTester = new ESLintTester(eslint);
+eslintTester.addRuleTest('rules/ng_no_service_method', {
+    valid: valid,
+    invalid: [{
+        code: 'app.service("Service", function(){});',
+        args: [1, 'eslint'],
+        errors: [{ message: 'You should prefer the factory() method instead of service()'}]
+    }, {
+        code: 'app.service("Service", [function(){}]);',
+        args: [1, /^eslint/],
+        errors: [{ message: 'You should prefer the factory() method instead of service()'}]
+    }]
+});
