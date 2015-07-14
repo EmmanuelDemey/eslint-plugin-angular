c32746d3a0fc98eb95a8c1abba8a5ad479ab171f
Brian Olore <brian.olore@adp.com>
2015-07-14 17:06:24 -0400

    better whitespace and less descriptive test function names

--- test/ng_karma_done.js
+++ test/ng_karma_done.js
@@ -14,34 +14,28 @@ eslintTester.addRuleTest('rules/ng_karma_done', {
   valid: [
     {
       code: '\
-          \
             it("with catch should reject if passed state does not exist", function (done) { \
               Service.save().catch(function () {                                            \
                 done();                                                                     \
               });                                                                           \
-              $scope.$digest();                                                             \
             });                                                                             \
           '
     },
     {
       code: '\
-            \
               it("with catch passes if done function is not named done", function (foo) { \
                 Service.save().catch(function () {                                        \
-                    foo();                                                                \
+                  foo();                                                                  \
                 });                                                                       \
-                $scope.$digest();                                                         \
               });                                                                         \
             '
     },
     {
       code: '\
-            \
               it("with then, passes if done function is not named done", function (foo) { \
                 Service.save().then(function () {                                         \
-                    foo();                                                                \
+                  foo();                                                                  \
                 });                                                                       \
-                $scope.$digest();                                                         \
               });                                                                         \
             '
     }
@@ -51,27 +45,21 @@ eslintTester.addRuleTest('rules/ng_karma_done', {
   invalid: [
     {
       code: '\
-        \
           it("should fail because there is a catch with no done", function() {      \
             Service.save().catch(function () {                                      \
-              expect(1).toEqual(1);                                                 \
+              doSomething();                                                        \
             });                                                                     \
-            $scope.$digest();                                                       \
           });                                                                       \
-        \
         ',
       errors: [{message: 'Spec contains a then/catch but doesn\'t define a done() function'}]
     },
     {
       code: '\
-        \
           it("should fail because there is a then with no done", function() {       \
             Service.save("paperwork").then(function () {                            \
-              expect(1).toEqual(1);                                                 \
+              doSomething();                                                        \
             });                                                                     \
-            $scope.$digest();                                                       \
           });                                                                       \
-        \
         ',
       errors: [{message: 'Spec contains a then/catch but doesn\'t define a done() function'}]
     }
