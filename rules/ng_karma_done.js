302fd704e01c2483e670c0ab893a605ca8d7dcb1
Brian Olore <brian.olore@adp.com>
2015-07-14 23:00:12 -0400

    refactor a bit

--- rules/ng_karma_done.js
+++ rules/ng_karma_done.js
@@ -18,34 +18,76 @@ module.exports = function (context) {
     }
   }
 
+
+  // if there is a .then or a .catch, it should call a DONE function
+  function checkDoneIsDefined(node) {
+    var source = context.getSource(node);
+    if (source.indexOf('.then') > -1 || source.indexOf('.catch') > -1) {
+      var doneFnName = getDoneFnName(node);
+      if (!doneFnName) {
+        context.report(node, 'Spec contains a then/catch but doesn\'t define a done() function');
+        return true;
+      }
+    }
+    return false;
+  }
+
   function getThen(node) {
+    var calledDone = false;
     if (node.arguments.length === 2) { // 'it' should always have 2 args, 2nd one being the function for the test
-                                          // TODO: unless people are getting too fancy on me
+      // TODO: unless people are getting too fancy on me
       var fe = node.arguments[1];
+      var doneFnName = getDoneFnName(node);
       if (fe.type === 'FunctionExpression') {
 
         // then or catch
         for ( var i = 0; i < fe.body.body.length; i++ ) {
           var thenOrCatch = fe.body.body[i].expression.callee.property.name;
           if (thenOrCatch === 'then' || thenOrCatch === 'catch') {
-            //console.log(fe.body);
-            //console.log('------');
-            ////console.log(fe.body.body[0]);
-            //console.log('######');
-            //console.log(fe.body.body[i].expression.arguments[0]);
+            console.log('bbbb', fe.body.body[0].expression);
+            console.log('rrrr', fe.body.body[1].expression);
             var bodyParts = fe.body.body[i].expression.arguments[0].body.body;
+            console.log('cccc', bodyParts);
             for ( var j = 0; j < bodyParts.length; j++) {
-              //console.log(bodyParts[j]);
               if (bodyParts[j].expression.callee.type === 'Identifier' &&
-                bodyParts[j].expression.callee.name === 'done') {
-                console.log('were good, we called done!');
+                bodyParts[j].expression.callee.name === doneFnName) {
+                calledDone = true;
+              } else {
+                console.log('aaaa', bodyParts[j].expression);
               }
             }
           }
         }
       }
     }
+    if (!calledDone) {
+      context.report(node, 'Done not called');
+    }
   }
+
+
+  // if it has a DONE function passed in, it should call it within a catch or then block
+  function checkDoneIsCalledProperly(node) {
+    var doneFnName = getDoneFnName(node);
+
+    if (doneFnName) {
+      var source = context.getSource(node);
+
+      // GOOD: see if function name is anywhere in the code beyond the param
+      // FIXME this fails if doneFnName is in the spec name
+      if (source.indexOf(doneFnName, source.indexOf(doneFnName) + 1) === -1) {
+        context.report(node, doneFnName + '() not called - this will hang karma');
+      }
+
+      // BETTER: make sure DONE function is used within a then/catch
+      if (true) {
+        getThen(node);
+        //context.report(node, "done() called, but not within then/catch");
+      }
+
+    }
+  }
+
 
   return {
 
@@ -56,32 +98,10 @@ module.exports = function (context) {
 
         if (isIt(node)) {
 
-          // if there is a .then or a .catch, it should call a DONE function
-          var source = context.getSource(node);
-          if (source.indexOf('.then') > -1 || source.indexOf('.catch') > -1) {
-            var doneFnName = getDoneFnName(node);
-            if (!doneFnName) {
-              context.report(node, 'Spec contains a then/catch but doesn\'t define a done() function');
-            }
-          }
+          var error = checkDoneIsDefined(node);
 
-          //// if it has a DONE function passed in, it should call it
-          //var doneFnName = getDoneFnName(node);
-          //if (doneFnName) {
-          //  var source = context.getSource(node);
-          //
-          //  // GOOD: see if function name is anywhere in the code beyond the param
-          //  // FIXME this fails if doneFnName is in the spec name
-          //  if (source.indexOf(doneFnName, source.indexOf(doneFnName) + 1) === -1) {
-          //    context.report(node, doneFnName + "() not called - this will hang karma");
-          //  }
-          //
-          //  // BETTER: make sure DONE function is used within a then/catch
-          //  if (true) {
-          //    getThen(node);
-          //    //context.report(node, "done() called, but not within then/catch");
-          //  }
-          //
+          //if (!error) {
+          //  checkDoneIsCalledProperly(node);
           //}
 
           //TODO: needs a $digest, $apply or flush
