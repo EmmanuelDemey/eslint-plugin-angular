8ee78027f67aa59c110b3a1756859c77263b0690
Emmanuel DEMEY <demey.emmanuel@gmail.com>
2015-07-23 17:04:50 +0200

    Issue #119 add .deferred

---
+++ rules/ng_deferred.js
@@ -0,0 +1,17 @@
+module.exports = function(context) {
+
+    'use strict';
+
+    return {
+
+        'MemberExpression': function(node) {
+            if(node.object.type === 'Identifier' && node.object.name === '$q'){
+                if(node.property.type === 'Identifier' && node.property.name === 'deferred'){
+                    context.report(node, 'You should not create a new promise with this syntax. Use the $q(function(resolve, reject){}) syntax.', {});
+                }
+
+            }
+        }
+    };
+
+};
