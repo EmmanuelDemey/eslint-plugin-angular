a79dbfccde15bc50d4a1a429c18f0847d88620b8
Emmanuel DEMEY <demey.emmanuel@gmail.com>
2015-07-25 15:47:11 +0200

    Issue #114 ng_on_watch only usefull with

--- test/ng_on_watch.js
+++ test/ng_on_watch.js
@@ -23,13 +23,14 @@ eslintTester.addRuleTest('rules/ng_on_watch', {
         '$scope.$on("$destroy", $scope.$on())',
         '$rootScope.$on("$destroy", $scope.$on())',
         '$scope.$on("$destroy", $rootScope.$on())',
-        '$rootScope.$on("$destroy", $rootScope.$on())'
+        '$rootScope.$on("$destroy", $rootScope.$on())',
+        'scope.$on()',
+        'scope.$watch()',
+        '$scope.$on()',
+        '$scope.$watch()'
+
     ],
     invalid: [
-        { code: 'scope.$on()', errors: [{ message: 'The "$on" call should be assigned to a variable, in order to be destroyed during the $destroy event'}] },
-        { code: 'scope.$watch()', errors: [{ message: 'The "$watch" call should be assigned to a variable, in order to be destroyed during the $destroy event'}] },
-        { code: '$scope.$on()', errors: [{ message: 'The "$on" call should be assigned to a variable, in order to be destroyed during the $destroy event'}] },
-        { code: '$scope.$watch()', errors: [{ message: 'The "$watch" call should be assigned to a variable, in order to be destroyed during the $destroy event'}] },
         { code: '$rootScope.$on()', errors: [{ message: 'The "$on" call should be assigned to a variable, in order to be destroyed during the $destroy event'}] },
         { code: '$rootScope.$watch()', errors: [{ message: 'The "$watch" call should be assigned to a variable, in order to be destroyed during the $destroy event'}] }
     ]
