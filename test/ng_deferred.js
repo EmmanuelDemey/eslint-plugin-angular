3c2adafe42163412b06eb8f58c7972b90e0d9aca
Remco Haszing <remcohaszing@gmail.com>
2015-08-22 20:51:51 +0200

    Check for $q.defer to report instead of $q.deferred

--- test/ng_deferred.js
+++ test/ng_deferred.js
@@ -15,6 +15,6 @@ eslintTester.run('ng_deferred', rule, {
         '$qs(function(){});'
     ],
     invalid: [
-        { code: 'var deferred = $q.deferred;', errors: [{ message: 'You should not create a new promise with this syntax. Use the $q(function(resolve, reject){}) syntax.'}] }
+        { code: 'var deferred = $q.defer();', errors: [{ message: 'You should not create a new promise with this syntax. Use the $q(function(resolve, reject){}) syntax.'}] }
     ]
 });
