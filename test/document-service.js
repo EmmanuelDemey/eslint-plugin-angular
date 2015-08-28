fbb9453839efededb80d112648e7a70bc13443d0
Tilman Potthof <tilmanpotthof@googlemail.com>
2015-08-28 18:20:26 +0200

    Issue #177 - add rules with new names and log warning for deprecated style

---
+++ test/document-service.js
@@ -0,0 +1,21 @@
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+var rule = require('../rules/document-service'),
+    RuleTester = require("eslint").RuleTester;
+
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+
+var eslintTester = new RuleTester();
+eslintTester.run('document-service', rule, {
+    valid: [
+        '$document[0].title = ""'
+    ],
+    invalid: [
+        { code: 'document.title', errors: [{ message: 'You should use the $document service instead of the default document object'}] },
+        { code: 'window.document.title', errors: [{ message: 'You should use the $document service instead of the default document object'}] }
+    ]
+});
---
+++ test/typecheck-array.js
@@ -0,0 +1,28 @@
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+var rule = require('../rules/typecheck-array'),
+    RuleTester = require("eslint").RuleTester;
+
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+
+var eslintTester = new RuleTester();
+eslintTester.run('typecheck-array', rule, {
+    valid: [
+        'angular.isArray([])'
+    ],
+    invalid: [
+        { code: 'Object.prototype.toString.call([]) === "[object Array]"', errors: [{ message: 'You should use the angular.isArray method'}] },
+        { code: '"[object Array]" === Object.prototype.toString.call([])', errors: [{ message: 'You should use the angular.isArray method'}] },
+        { code: 'variable === "[object Array]"', errors: [{ message: 'You should use the angular.isArray method'}] },
+        { code: '"[object Array]" === variable', errors: [{ message: 'You should use the angular.isArray method'}] },
+        { code: 'Object.prototype.toString.call([]) !== "[object Array]"', errors: [{ message: 'You should use the angular.isArray method'}] },
+        { code: '"[object Array]" !== typeof []', errors: [{ message: 'You should use the angular.isArray method'}] },
+        { code: 'variable !== "[object Array]"', errors: [{ message: 'You should use the angular.isArray method'}] },
+        { code: '"[object Array]" !== variable', errors: [{ message: 'You should use the angular.isArray method'}] },
+        { code: 'Array.isArray(variable)', errors: [{ message: 'You should use the angular.isArray method'}] }
+    ]
+});
