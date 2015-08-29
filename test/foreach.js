8a57ff86bc3045c38bb2c6db37b8830393939d8f
Emmanuel DEMEY <demey.emmanuel@gmail.com>
2015-08-29 10:36:02 +0200

    Revert "Revert "Issue 177/rule names inconsistent with the standard es lint r…"

---
+++ test/foreach.js
@@ -0,0 +1,20 @@
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+var rule = require('../rules/foreach'),
+    RuleTester = require("eslint").RuleTester;
+
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+
+var eslintTester = new RuleTester();
+eslintTester.run('foreach', rule, {
+    valid: [
+        'angular.forEach(variable, function(){})'
+    ],
+    invalid: [
+        { code: 'variable.forEach(function(){})', errors: [{ message: 'You should use the angular.forEach method'}] }
+    ]
+});
