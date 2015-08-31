8cd80b0ef9f4411ebfe154c8e02ccb2cee4bb116
Remco Haszing <remcohaszing@gmail.com>
2015-08-31 21:31:23 +0200

    Add a rule for setting a component per file limit

---
+++ rules/component-limit.js
@@ -0,0 +1,33 @@
+module.exports = function(context) {
+
+    'use strict';
+
+    var utils = require('./utils/utils');
+    var limit = context.options[0] || 1;
+
+    var components = [];
+
+    return {
+
+        'CallExpression': function(node) {
+            if(utils.isAngularComponent(node)) {
+                components.push(node);
+            }
+        },
+
+        'Program:exit': function() {
+            if(components.length > limit) {
+                components.forEach(function(node) {
+                    context.report(node, 'There may be at most {{limit}} AngularJS {{component}} per file', {
+                        limit: limit,
+                        component: limit === 1 ? 'component' : 'components'
+                    });
+                })
+            }
+        }
+    };
+};
+
+module.exports.schema = [{
+    type: 'integer'
+}];
