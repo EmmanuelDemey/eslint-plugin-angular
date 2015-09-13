0ebd36db675f36f7575b169440f2c07306c362c7
Remco Haszing <remcohaszing@gmail.com>
2015-09-13 14:42:29 +0200

    Restructure utils
    Previously all was directly assigned to the module.exports object.
    
    Now it is somewhat more structured.
    
    First properties are listed.
    Next modules.exports is defined, giving an overview of the module.
    Last named functions are declared. This provides better stacktraces.

--- rules/utils/utils.js
+++ rules/utils/utils.js
@@ -1,6 +1,59 @@
 'use strict';
 
 
+var scopeProperties = [
+    '$id',
+    '$parent',
+    '$root',
+    '$destroy',
+    '$broadcast',
+    '$emit',
+    '$on',
+    '$applyAsync',
+    '$apply',
+    '$evalAsync',
+    '$eval',
+    '$digest',
+    '$watchCollection',
+    '$watchGroup',
+    '$watch',
+    '$new'
+];
+
+
+module.exports = {
+    // Properties
+    scopeProperties: scopeProperties,
+
+    // Functions
+    convertPrefixToRegex: convertPrefixToRegex,
+    convertStringToRegex: convertStringToRegex,
+    isTypeOfStatement: isTypeOfStatement,
+    isToStringStatement: isToStringStatement,
+    isArrayType: isArrayType,
+    isFunctionType: isFunctionType,
+    isIdentifierType: isIdentifierType,
+    isMemberExpression: isMemberExpression,
+    isLiteralType: isLiteralType,
+    isEmptyFunction: isEmptyFunction,
+    isRegexp: isRegexp,
+    isStringRegexp: isStringRegexp,
+    isAngularComponent: isAngularComponent,
+    isAngularControllerDeclaration: isAngularControllerDeclaration,
+    isAngularFilterDeclaration: isAngularFilterDeclaration,
+    isAngularDirectiveDeclaration: isAngularDirectiveDeclaration,
+    isAngularServiceDeclaration: isAngularServiceDeclaration,
+    isAngularModuleDeclaration: isAngularModuleDeclaration,
+    isAngularModuleGetter: isAngularModuleGetter,
+    isAngularRunSection: isAngularRunSection,
+    isAngularConfigSection: isAngularConfigSection,
+    isRouteDefinition: isRouteDefinition,
+    isUIRouterStateDefinition: isUIRouterStateDefinition,
+    findIdentiferInScope: findIdentiferInScope,
+    getControllerDefinition: getControllerDefinition
+};
+
+
 // this will recursively grab the callee until it hits an Identifier
 function getCallingIdentifier(calleeObject) {
     if (calleeObject.type && calleeObject.type === 'Identifier') {
@@ -12,190 +65,184 @@ function getCallingIdentifier(calleeObject) {
     return null;
 }
 
-module.exports = {
-
-    convertPrefixToRegex: function(prefix) {
-        if (typeof prefix !== 'string') {
-            return prefix;
-        }
-
-        if (prefix[0] === '/' && prefix[prefix.length - 1] === '/') {
-            prefix = prefix.substring(1, prefix.length - 2);
-        }
-
-        return new RegExp(prefix + '.*');
-    },
-
-    convertStringToRegex: function(string) {
-        if (string[0] === '/' && string[string.length - 1] === '/') {
-            string = string.substring(1, string.length - 2);
-        }
-        return new RegExp(string);
-    },
-
-    isTypeOfStatement: function(node) {
-        return node.type === 'Identifier' || (node.type === 'UnaryExpression' && node.operator === 'typeof');
-    },
-
-    isToStringStatement: function(node) {
-        return node.type === 'CallExpression' &&
-            node.callee.type === 'MemberExpression' &&
-            node.callee.object.type === 'MemberExpression' &&
-            node.callee.object.property.name === 'toString' &&
-            node.callee.property.name === 'call' &&
-            node.callee.object.object.type === 'MemberExpression' &&
-            node.callee.object.object.object.name === 'Object' &&
-            node.callee.object.object.property.name === 'prototype';
-    },
-
-    isArrayType: function(node) {
-        return node !== undefined && node.type === 'ArrayExpression';
-    },
-
-    isFunctionType: function(node) {
-        return node !== undefined && node.type === 'FunctionExpression';
-    },
-
-    isIdentifierType: function(node) {
-        return node !== undefined && node.type === 'Identifier';
-    },
-
-    isMemberExpression: function(node) {
-        return node !== undefined && node.type === 'MemberExpression';
-    },
-
-    isLiteralType: function(node) {
-        return node !== undefined && node.type === 'Literal';
-    },
-
-    isEmptyFunction: function(fn) {
-        return fn.body.body.length === 0;
-    },
-
-    isRegexp: function(regexp) {
-        return toString.call(regexp) === '[object RegExp]';
-    },
-
-    isStringRegexp: function(string) {
-        return string[0] === '/' && string[string.length - 1] === '/';
-    },
-
-    isAngularComponent: function(node) {
-        return node.arguments !== undefined && node.arguments.length === 2 && this.isLiteralType(node.arguments[0]) && (this.isIdentifierType(node.arguments[1]) || this.isFunctionType(node.arguments[1]) || this.isArrayType(node.arguments[1]));
-    },
-
-    isAngularControllerDeclaration: function(node) {
-        return this.isAngularComponent(node) &&
-            this.isMemberExpression(node.callee) &&
-            node.callee.property.name === 'controller';
-    },
-
-    isAngularFilterDeclaration: function(node) {
-        return this.isAngularComponent(node) &&
-            this.isMemberExpression(node.callee) &&
-            node.callee.property.name === 'filter';
-    },
-
-    isAngularDirectiveDeclaration: function(node) {
-        return this.isAngularComponent(node) &&
-            this.isMemberExpression(node.callee) &&
-            node.callee.property.name === 'directive';
-    },
-
-    isAngularServiceDeclaration: function(node) {
-        return this.isAngularComponent(node) &&
-            this.isMemberExpression(node.callee) &&
-            node.callee.object.name !== '$provide' &&
-            (node.callee.property.name === 'provider' ||
-             node.callee.property.name === 'service' ||
-             node.callee.property.name === 'factory' ||
-             node.callee.property.name === 'constant' ||
-             node.callee.property.name === 'value');
-    },
-
-    isAngularModuleDeclaration: function(node) {
-        return this.isAngularComponent(node) &&
-            this.isMemberExpression(node.callee) &&
-            node.callee.property.name === 'module';
-    },
-
-    isAngularModuleGetter: function(node) {
-        return node.arguments !== undefined &&
-            node.arguments.length > 0 &&
-            this.isLiteralType(node.arguments[0]) &&
-            node.callee.type === 'MemberExpression' &&
-            node.callee.property.name === 'module';
-    },
-
-    isAngularRunSection: function(node) {
-        return this.isMemberExpression(node.callee) &&
-            node.callee.property.type === 'Identifier' &&
-            node.callee.property.name === 'run' &&
-            (node.callee.object.type === 'Identifier' &&
-             node.callee.object.name !== 'mocha');
-    },
-
-    isAngularConfigSection: function(node) {
-        return this.isMemberExpression(node.callee) &&
-            node.callee.property.type === 'Identifier' &&
-            node.callee.property.name === 'config';
-    },
-
-    isRouteDefinition: function(node) {
-        // the route def function is .when(), so when we find that, go up through the chain and make sure
-        // $routeProvider is the calling object
-        if (node.callee.property && node.callee.property.name === 'when') {
-            var callObject = getCallingIdentifier(node.callee.object);
-            return callObject && callObject.name === '$routeProvider';
-        }
-        return false;
-    },
-
-    isUIRouterStateDefinition: function(node) {
-        // the state def function is .state(), so when we find that, go up through the chain and make sure
-        // $stateProvider is the calling object
-        if (node.callee.property && node.callee.property.name === 'state') {
-            var callObject = getCallingIdentifier(node.callee.object);
-            return callObject && callObject.name === '$stateProvider';
-        }
-        return false;
-    },
-
-    scopeProperties: ['$id', '$parent', '$root', '$destroy', '$broadcast', '$emit', '$on', '$applyAsync', '$apply',
-     '$evalAsync', '$eval', '$digest', '$watchCollection', '$watchGroup', '$watch', '$new'],
-
-    findIdentiferInScope: function(context, identifier) {
-        var identifierNode = null;
-        context.getScope().variables.forEach(function(variable) {
-            if (variable.name === identifier.name) {
-                identifierNode = variable.defs[0].node;
-                if (identifierNode.type === 'VariableDeclarator') {
-                    identifierNode = identifierNode.init;
-                }
-            }
-        });
-        return identifierNode;
-    },
-
-    getControllerDefinition: function(context, node) {
-        var controllerArg = node.arguments[1];
-
-        // Three ways of creating a controller function: function expression,
-        // variable name that references a function, and an array with a function
-        // as the last item
-        if (this.isFunctionType(controllerArg)) {
-            return controllerArg;
-        }
-        if (this.isArrayType(controllerArg)) {
-            controllerArg = controllerArg.elements[controllerArg.elements.length - 1];
-
-            if (this.isIdentifierType(controllerArg)) {
-                return this.findIdentiferInScope(context, controllerArg);
-            }
-            return controllerArg;
-        }
-        if (this.isIdentifierType(controllerArg)) {
-            return this.findIdentiferInScope(context, controllerArg);
-        }
+function convertPrefixToRegex(prefix) {
+    if (typeof prefix !== 'string') {
+        return prefix;
     }
-};
+
+    if (prefix[0] === '/' && prefix[prefix.length - 1] === '/') {
+        prefix = prefix.substring(1, prefix.length - 2);
+    }
+
+    return new RegExp(prefix + '.*');
+}
+
+function convertStringToRegex(string) {
+    if (string[0] === '/' && string[string.length - 1] === '/') {
+        string = string.substring(1, string.length - 2);
+    }
+    return new RegExp(string);
+}
+
+function isTypeOfStatement(node) {
+    return node.type === 'Identifier' || (node.type === 'UnaryExpression' && node.operator === 'typeof');
+}
+
+function isToStringStatement(node) {
+    return node.type === 'CallExpression' &&
+        node.callee.type === 'MemberExpression' &&
+        node.callee.object.type === 'MemberExpression' &&
+        node.callee.object.property.name === 'toString' &&
+        node.callee.property.name === 'call' &&
+        node.callee.object.object.type === 'MemberExpression' &&
+        node.callee.object.object.object.name === 'Object' &&
+        node.callee.object.object.property.name === 'prototype';
+}
+
+function isArrayType(node) {
+    return node !== undefined && node.type === 'ArrayExpression';
+}
+
+function isFunctionType(node) {
+    return node !== undefined && node.type === 'FunctionExpression';
+}
+
+function isIdentifierType(node) {
+    return node !== undefined && node.type === 'Identifier';
+}
+
+function isMemberExpression(node) {
+    return node !== undefined && node.type === 'MemberExpression';
+}
+
+function isLiteralType(node) {
+    return node !== undefined && node.type === 'Literal';
+}
+
+function isEmptyFunction(fn) {
+    return fn.body.body.length === 0;
+}
+
+function isRegexp(regexp) {
+    return toString.call(regexp) === '[object RegExp]';
+}
+
+function isStringRegexp(string) {
+    return string[0] === '/' && string[string.length - 1] === '/';
+}
+
+function isAngularComponent(node) {
+    return node.arguments !== undefined && node.arguments.length === 2 && isLiteralType(node.arguments[0]) && (isIdentifierType(node.arguments[1]) || isFunctionType(node.arguments[1]) || isArrayType(node.arguments[1]));
+}
+
+function isAngularControllerDeclaration(node) {
+    return isAngularComponent(node) &&
+        isMemberExpression(node.callee) &&
+        node.callee.property.name === 'controller';
+}
+
+function isAngularFilterDeclaration(node) {
+    return isAngularComponent(node) &&
+        isMemberExpression(node.callee) &&
+        node.callee.property.name === 'filter';
+}
+
+function isAngularDirectiveDeclaration(node) {
+    return isAngularComponent(node) &&
+        isMemberExpression(node.callee) &&
+        node.callee.property.name === 'directive';
+}
+
+function isAngularServiceDeclaration(node) {
+    return isAngularComponent(node) &&
+        isMemberExpression(node.callee) &&
+        node.callee.object.name !== '$provide' &&
+        (node.callee.property.name === 'provider' ||
+         node.callee.property.name === 'service' ||
+         node.callee.property.name === 'factory' ||
+         node.callee.property.name === 'constant' ||
+         node.callee.property.name === 'value');
+}
+
+function isAngularModuleDeclaration(node) {
+    return isAngularComponent(node) &&
+        isMemberExpression(node.callee) &&
+        node.callee.property.name === 'module';
+}
+
+function isAngularModuleGetter(node) {
+    return node.arguments !== undefined &&
+        node.arguments.length > 0 &&
+        isLiteralType(node.arguments[0]) &&
+        node.callee.type === 'MemberExpression' &&
+        node.callee.property.name === 'module';
+}
+
+function isAngularRunSection(node) {
+    return isMemberExpression(node.callee) &&
+        node.callee.property.type === 'Identifier' &&
+        node.callee.property.name === 'run' &&
+        (node.callee.object.type === 'Identifier' &&
+         node.callee.object.name !== 'mocha');
+}
+
+function isAngularConfigSection(node) {
+    return isMemberExpression(node.callee) &&
+     node.callee.property.type === 'Identifier' &&
+     node.callee.property.name === 'config';
+}
+
+function isRouteDefinition(node) {
+    // the route def function is .when(), so when we find that, go up through the chain and make sure
+    // $routeProvider is the calling object
+    if (node.callee.property && node.callee.property.name === 'when') {
+        var callObject = getCallingIdentifier(node.callee.object);
+        return callObject && callObject.name === '$routeProvider';
+    }
+    return false;
+}
+
+function isUIRouterStateDefinition(node) {
+    // the state def function is .state(), so when we find that, go up through the chain and make sure
+    // $stateProvider is the calling object
+    if (node.callee.property && node.callee.property.name === 'state') {
+        var callObject = getCallingIdentifier(node.callee.object);
+        return callObject && callObject.name === '$stateProvider';
+    }
+    return false;
+}
+
+function findIdentiferInScope(context, identifier) {
+    var identifierNode = null;
+    context.getScope().variables.forEach(function(variable) {
+        if (variable.name === identifier.name) {
+            identifierNode = variable.defs[0].node;
+            if (identifierNode.type === 'VariableDeclarator') {
+                identifierNode = identifierNode.init;
+            }
+        }
+    });
+    return identifierNode;
+}
+
+function getControllerDefinition(context, node) {
+    var controllerArg = node.arguments[1];
+
+    // Three ways of creating a controller function: function expression,
+    // variable name that references a function, and an array with a function
+    // as the last item
+    if (isFunctionType(controllerArg)) {
+        return controllerArg;
+    }
+    if (isArrayType(controllerArg)) {
+        controllerArg = controllerArg.elements[controllerArg.elements.length - 1];
+
+        if (isIdentifierType(controllerArg)) {
+            return findIdentiferInScope(context, controllerArg);
+        }
+        return controllerArg;
+    }
+    if (isIdentifierType(controllerArg)) {
+        return findIdentiferInScope(context, controllerArg);
+    }
+}
