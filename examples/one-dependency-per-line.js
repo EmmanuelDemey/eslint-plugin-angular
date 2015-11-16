a2ea7008f3294d55430d0da71efe094ccb7ed902
palortoff <github@larsflitter.de>
2015-11-16 10:21:41 +0100

    Add rule to test that only one dependency is listed per line

---
+++ examples/one-dependency-per-line.js
@@ -0,0 +1,71 @@
+// example - valid: true
+app.controller('MyController', MyController);
+
+function MyController($http,
+                      $q) {
+}
+
+// example - valid: true
+app.controller('MyController', function($http,
+                                        $q) {
+    });
+
+
+// example - valid: true
+
+app.controller('MyController', [
+    '$http',
+    '$q',
+    function($http,
+             $q) {
+    }]);
+
+// example - valid: true
+app.controller('MyController', [
+    '$http',
+    '$q',
+    MyController]);
+
+function MyController($http,
+                      $q) {
+}
+
+// example - valid: false, errorMessage: "Do not use multiple dependencies in one line"
+app.controller('MyController', MyController);
+
+function MyController($http, $q) {}
+
+// example - valid: false, errorMessage: "Do not use multiple dependencies in one line"
+app.controller('MyController', function($http, $q) {});
+
+// example - valid: false, errorMessage: "Do not use multiple dependencies in one line"
+
+app.controller('MyController', ['$http','$q',
+    function($http,
+             $q) {
+    }]);
+
+// example - valid: false, errorMessage: "Do not use multiple dependencies in one line"
+
+app.controller('MyController', [
+    '$http',
+    '$q',
+    function($http, $q) {}]);
+
+// example - valid: false, errorMessage: "Do not use multiple dependencies in one line"
+
+app.controller('MyController', ['$http', '$q', MyController]);
+
+function MyController($http,
+                      $q) {}
+
+
+// example - valid: false, errorMessage: "Do not use multiple dependencies in one line"
+
+app.controller('MyController', [
+    '$http',
+    '$q',
+    MyController]);
+
+function MyController($http, $q) {}
+
