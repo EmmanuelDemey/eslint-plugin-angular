18151ec2789dd7ae690f75850ab37a6d26598c6b
Remco Haszing <remcohaszing@gmail.com>
2015-12-06 16:17:06 +0100

    Add new dumb-inject rule

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ rulesConfiguration.addRule('di-unused', 0);
 rulesConfiguration.addRule('directive-name', 0);
 rulesConfiguration.addRule('directive-restrict', 0);
 rulesConfiguration.addRule('document-service', 2);
+rulesConfiguration.addRule('dumb-inject', 2);
 rulesConfiguration.addRule('empty-controller', 0);
 rulesConfiguration.addRule('file-name', 0);
 rulesConfiguration.addRule('filter-name', 0);
