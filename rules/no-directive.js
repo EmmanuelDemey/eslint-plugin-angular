45a68238f6e5c326a521f6562c53e1df25784d1d
Emmanuel DEMEY <demey.emmanuel@gmail.com>
2015-12-09 17:11:43 +0100

    Solve ESLint issues

--- rules/no-directive.js
+++ rules/no-directive.js
@@ -1,6 +1,6 @@
 /*
- * Since AngularJS 1.5, we can use a new API when creating directives. This API will help the migration to the next version of the framework 
- * 
+ * Since AngularJS 1.5, we can use a new API when creating directives. This API will help the migration to the next version of the framework
+ *
  * @version 0.16.0
  * @category bestPractice
  * @sinceAngularVersion 1.5
@@ -9,15 +9,14 @@
 
 var angularRule = require('./utils/angular-rule');
 
-
 module.exports = angularRule(function(context) {
     function report(node) {
         context.report(node, 'Directive should be implemented with the component method', {});
     }
 
     return {
-        'angular:directive': function(callExpression, fn) {
-           report(callExpression);
+        'angular:directive': function(callExpression) {
+            report(callExpression);
         }
     };
 });
