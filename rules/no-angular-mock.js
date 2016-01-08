8378f42649c8c9b6a38f9d04335d177a3a03dc40
Austin Wood <austin.wood@globality.com>
2016-01-07 20:33:21 -0800

    Create note about adding ESLint globals for angular.mock methods

--- rules/no-angular-mock.js
+++ rules/no-angular-mock.js
@@ -6,6 +6,25 @@
  *
  * @version 0.2.0
  * @category angularWrapper
+ *
+ * NOTE: While this rule does enforce the use of `angular.mock` methods to be used
+ * in the object window, the `eslint` rule no-undef (http://eslint.org/docs/rules/no-undef.html)
+ * may prevent you from using undefined global variables such as those provided by
+ * `angular.mock`. The current fix for this is to simply add all of the `angular.mock`
+ * object methods to your `eslint` globals:
+ *
+ * "globals": {
+ *   "angular": false,
+ *   "module": false,
+ *   "inject": false
+ * }
+ *
+ * At this time (01/06/2016), there is no way to add globals for a specific environment
+ * in `eslint`, although it is an accepted feature (https://github.com/eslint/eslint/issues/4782)
+ * and should exist sometime in the future.
+ *
+ * Check here(https://github.com/Gillespie59/eslint-plugin-angular/issues/330)
+ * for more information on this topic.
  */
 'use strict';
 
