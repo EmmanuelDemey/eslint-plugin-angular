c6d600eef6bf5ee63d30890da13d98371ac1710f
Tilman Potthof <tilmanpotthof@googlemail.com>
2016-01-11 18:47:18 +0100

    Rewrite di rule with the angularRule ($inject excluded) (#329)

--- rules/di.js
+++ rules/di.js
@@ -10,82 +10,60 @@
 
 var utils = require('./utils/utils');
 
-module.exports = function(context) {
-    var angularNamedObjectList = ['factory', 'service', 'provider', 'controller', 'filter', 'directive'];
+var angularRule = require('./utils/angular-rule');
 
-    function report(node, syntax) {
+
+module.exports = angularRule(function(context) {
+    var syntax = context.options[0];
+
+    function report(node) {
         context.report(node, 'You should use the {{syntax}} syntax for DI', {
             syntax: syntax
         });
     }
 
-    var noninjectedFunctions = {};
-    var injectedFunctions = [];
+    function checkDi(callee, fn) {
+        if (!fn || !fn.params) {
+            return;
+        }
 
-    function checkDi(syntax, node, param) {
-        if (syntax === 'function' && (!utils.isFunctionType(param) && !utils.isIdentifierType(param))) {
-            report(node, syntax);
-        } else if (syntax === 'array') {
-            if (utils.isArrayType(param)) {
-                var fn = param.elements[param.elements.length - 1];
-                if (utils.isFunctionType(fn) && fn.params.length !== param.elements.length - 1) {
+        if (syntax === 'array') {
+            if (utils.isArrayType(fn.parent)) {
+                if (fn.parent.elements.length - 1 !== fn.params.length) {
                     context.report(fn, 'The signature of the method is incorrect', {});
+                    return;
                 }
-            } else if (!(utils.isFunctionType(param) && param.params.length === 0)) {
-                report(node, syntax);
-            }
-        } else if (syntax === '$inject') {
-            if (utils.isIdentifierType(param)) {
-                noninjectedFunctions[param.name] = node;
             } else {
-                report(node, syntax);
+                if (fn.params.length === 0) {
+                    return;
+                }
+                report(fn);
             }
         }
-    }
 
-    function maybeNoteInjection(syntax, node) {
-        if (syntax === '$inject' && node.left && node.left.property &&
-            ((utils.isLiteralType(node.left.property) && node.left.property.value === '$inject') ||
-             (utils.isIdentifierType(node.left.property) && node.left.property.name === '$inject'))) {
-            injectedFunctions.push(node.left.object.name);
-        }
-    }
-
-    function verifyInjections(syntax) {
-        if (syntax === '$inject') {
-            injectedFunctions.forEach(function(f) {
-                delete noninjectedFunctions[f];
-            });
-
-            for (var func in noninjectedFunctions) {
-                report(noninjectedFunctions[func], syntax);
+        if (syntax === 'function') {
+            if (utils.isArrayType(fn.parent)) {
+                report(fn);
             }
         }
     }
 
     return {
-
-        CallExpression: function(node) {
-            if (utils.isAngularComponent(node) && node.callee.type === 'MemberExpression' && angularNamedObjectList.indexOf(node.callee.property.name) >= 0) {
-                /**
-                * Check AngularJS components using functions with two parameters : name and constructor
-                */
-                checkDi(context.options[0], node, node.arguments[1]);
-            } else if (utils.isAngularRunSection(node) || utils.isAngularConfigSection(node)) {
-                /**
-                * Check AngularJS components using functions with one parameter : the constructor
-                */
-                checkDi(context.options[0], node, node.arguments[0]);
-            }
-        },
-        AssignmentExpression: function(node) {
-            maybeNoteInjection(context.options[0], node);
-        },
-        'Program:exit': function() {
-            verifyInjections(context.options[0]);
+        'angular:animation': checkDi,
+        'angular:config': checkDi,
+        'angular:controller': checkDi,
+        'angular:directive': checkDi,
+        'angular:factory': checkDi,
+        'angular:filter': checkDi,
+        'angular:inject': checkDi,
+        'angular:run': checkDi,
+        'angular:service': checkDi,
+        'angular:provider': function(callee, providerFn, $get) {
+            checkDi(null, providerFn);
+            checkDi(null, $get);
         }
     };
-};
+});
 
 module.exports.schema = [{
     enum: [
