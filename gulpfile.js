3091e11b17a74cb05f7f6cd6087648677602b18d
Gilles Piou <gilles.piou@gmail.com>
2024-04-27 17:02:14 +0800

    Update test libraries

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,6 @@
 
 var gulp = require('gulp');
 var eslint = require('gulp-eslint');
-var istanbul = require('gulp-istanbul');
-var mocha = require('gulp-mocha');
 var docs = require('./scripts/docs.js');
 
 
@@ -14,19 +12,6 @@ gulp.task('quality', function() {
         .pipe(eslint.failAfterError());
 });
 
-gulp.task('test', function(cb) {
-    gulp.src(['index.js', 'rules/**/*.js'])
-        .pipe(istanbul()) // Covering files
-        .pipe(istanbul.hookRequire()) // Force `require` to return covered files
-        .on('finish', function() {
-            gulp.src(['test/**/*.js'])
-                .pipe(mocha())
-                .pipe(istanbul.writeReports()) // Creating the reports after tests runned
-                .on('end', cb);
-        });
-});
-
-
 gulp.task('docs', function(done) {
     docs.updateReadme('README.md');
     docs.createDocFiles();
@@ -34,4 +19,4 @@ gulp.task('docs', function(done) {
     done();
 });
 
-gulp.task('default', gulp.series('quality', 'docs', 'test'));
+gulp.task('default', gulp.series('quality', 'docs'));
