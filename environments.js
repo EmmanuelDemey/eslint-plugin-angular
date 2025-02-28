4dbc8acf79ab42c438c217e494f8138fe221afcd
Alaa Jerbi <alaa.jerbi@medius.com>
2025-02-28 22:02:04 +0100

    Upgrade to eslint 9 and use flat config

--- environments.js
+++ environments.js
@@ -2,27 +2,33 @@
 
 module.exports = {
     angular: {
-        globals: {
-            angular: true
+        languageOptions: {
+            globals: {
+                angular: true
+            }
         }
     },
     // https://docs.angularjs.org/api/ngMock
     mocks: {
-        globals: {
-            angular: true,
-            inject: true,
-            module: true
+        languageOptions: {
+            globals: {
+                angular: true,
+                inject: true,
+                module: true
+            }
         }
     },
     // http://www.protractortest.org/#/api
     protractor: {
-        globals: {
-            element: true,
-            $: true,
-            $$: true,
-            browser: true,
-            by: true,
-            protractor: true
+        languageOptions: {
+            globals: {
+                element: true,
+                $: true,
+                $$: true,
+                browser: true,
+                by: true,
+                protractor: true
+            }
         }
     }
 };
--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 'use strict';
 
 /* eslint-disable global-require */
+const {fixupPluginRules} = require('@eslint/compat');
 
 var fs = require('fs');
 var path = require('path');
-
+var environments = require('./environments');
 var rules = {};
 var ruleDir = path.join(__dirname, 'rules');
 
@@ -15,63 +16,65 @@ fs.readdirSync(ruleDir).forEach(function(name) {
     }
 });
 
-module.exports = {
-    rules: rules,
-    environments: require('./environments'),
-    configs: {
-        johnpapa: {
-            plugins: [
-                'angular'
-            ],
-            rules: {
-                'angular/component-name': 2,
-                'angular/constant-name': 2,
-                'angular/controller-as-route': 2,
-                'angular/controller-as-vm': 2,
-                'angular/controller-as': 2,
-                'angular/controller-name': 2,
-                'angular/directive-name': 2,
-                'angular/directive-restrict': 2,
-                'angular/document-service': 2,
-                'angular/factory-name': 2,
-                'angular/file-name': 2,
-                'angular/filter-name': 2,
-                'angular/function-type': 2,
-                'angular/interval-service': 2,
-                'angular/module-getter': 2,
-                'angular/module-name': 2,
-                'angular/module-setter': 2,
-                'angular/no-run-logic': 2,
-                'angular/no-service-method': 2,
-                'angular/provider-name': 2,
-                'angular/service-name': 2,
-                'angular/timeout-service': 2,
-                'angular/value-name': 2,
-                'angular/window-service': 2
-            }
+var pluginAngular = fixupPluginRules(
+    Object.assign({}, environments, {rules})
+);
+
+pluginAngular.configs = {
+    johnpapa: {
+        plugins: {
+            angular: pluginAngular
         },
-        bestpractices: {
-            plugins: [
-                'angular'
-            ],
-            rules: {
-                'angular/component-name': 2,
-                'angular/constant-name': 2,
-                'angular/controller-as-route': 2,
-                'angular/controller-as-vm': 2,
-                'angular/controller-as': 2,
-                'angular/deferred': 2,
-                'angular/di-unused': 2,
-                'angular/directive-restrict': 2,
-                'angular/empty-controller': 2,
-                'angular/no-controller': 2,
-                'angular/no-inline-template': 2,
-                'angular/no-run-logic': 2,
-                'angular/no-service-method': 2,
-                'angular/no-services': 2,
-                'angular/on-watch': 2,
-                'angular/prefer-component': 2
-            }
+        rules: {
+            'angular/component-name': 2,
+            'angular/constant-name': 2,
+            'angular/controller-as-route': 2,
+            'angular/controller-as-vm': 2,
+            'angular/controller-as': 2,
+            'angular/controller-name': 2,
+            'angular/directive-name': 2,
+            'angular/directive-restrict': 2,
+            'angular/document-service': 2,
+            'angular/factory-name': 2,
+            'angular/file-name': 2,
+            'angular/filter-name': 2,
+            'angular/function-type': 2,
+            'angular/interval-service': 2,
+            'angular/module-getter': 2,
+            'angular/module-name': 2,
+            'angular/module-setter': 2,
+            'angular/no-run-logic': 2,
+            'angular/no-service-method': 2,
+            'angular/provider-name': 2,
+            'angular/service-name': 2,
+            'angular/timeout-service': 2,
+            'angular/value-name': 2,
+            'angular/window-service': 2
+        }
+    },
+    bestpractices: {
+        plugins: {
+            angular: pluginAngular
+        },
+        rules: {
+            'angular/component-name': 2,
+            'angular/constant-name': 2,
+            'angular/controller-as-route': 2,
+            'angular/controller-as-vm': 2,
+            'angular/controller-as': 2,
+            'angular/deferred': 2,
+            'angular/di-unused': 2,
+            'angular/directive-restrict': 2,
+            'angular/empty-controller': 2,
+            'angular/no-controller': 2,
+            'angular/no-inline-template': 2,
+            'angular/no-run-logic': 2,
+            'angular/no-service-method': 2,
+            'angular/no-services': 2,
+            'angular/on-watch': 2,
+            'angular/prefer-component': 2
         }
     }
 };
+
+module.exports = pluginAngular;
